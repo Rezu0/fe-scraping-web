978de0f4281ccd16971fc5a1827d4abbed76c3b2
Rezu0 <yunianrezky031@gmail.com>
2025-03-12 13:39:34 +0700

    add: penambahan API untuk data videos dan render component nya

--- src/component/card/MainCardImageComponent.jsx
+++ src/component/card/MainCardImageComponent.jsx
@@ -1,22 +1,66 @@
-function MainCardImageComponent() {
+import { useEffect, useState } from "react";
+import { mockAPIVideos } from "../../utils/mockAPI/mockAPIVideos";
+import { formatDateToString } from '../../utils/date/handlerDate';
+import { useSearchParams } from "react-router-dom";
+import { toast } from "sonner";
+
+function MainCardImageComponent({ sendDataToParent }) {
+  const [isContentVideos, setContentVideos] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [searchParams] = useSearchParams();
+  
+  useEffect(() => {
+    const fetchContentVideos = async () => {
+      setLoading(true);
+      try {
+        const page = searchParams.get('page') ?? 1;
+        const response = await mockAPIVideos({ page: page });
+        if (!response.status) toast(response.message);
+
+        setContentVideos(response?.data);
+        sendDataToParent(response?.pagination);
+        return;
+      } catch (err) {
+        toast('An error occurred while fetching mock Videos (err1 GET)');
+        return;
+      } finally {
+        setLoading(false); // jika tidak ingin ada loading atau pun refresh ke atas maka hapus loading nya
+      }
+    }
+
+    fetchContentVideos();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setContentVideos, searchParams])
+
   return (
     <>
-    
-      <div className="w-full max-w-xs shadow-md overflow-hidden">
-        {/* Gambar */}
-        <img 
-          src="https://www.xtapes.io/img/paris_amterdam_capitales_du_sexe.jpg" 
-          alt="Thumbnail"
-          className="w-full h-48 object-cover rounded-md"
-        />
-
-        {/* Konten di Bawah Gambar */}
-        <div className="py-1">
-          <h3 className="text-md font-semibold text-adultdesu-navbartext line-clamp-2">Paris-Amsterdam Capitales du Sexe – Full Movie (1998)</h3>
-          <p className="text-sm text-gray-500">10 Maret 2025</p>
-          <p className="text-sm text-gray-500">1 Jam 30 Menit</p>
+      {isLoading ? (
+        <div className="w-full text-center text-adultdesu-navbartext">
+          Loading...
         </div>
-      </div>
+      ) : (
+        isContentVideos?.map((item, index) => (
+          <div 
+            key={index}
+            className="w-full max-w-xs shadow-md overflow-hidden group"
+          >
+            {/* Gambar */}
+            <img 
+              srcSet={item.image}
+              alt="Thumbnail"
+              className="w-full h-48 object-cover rounded-md hover:opacity-50 cursor-pointer duration-300"
+              loading="lazy"
+            />
+  
+            {/* Konten di Bawah Gambar */}
+            <div className="py-1">
+              <h3 className="text-md font-semibold text-adultdesu-navbartext line-clamp-2 group-hover:underline duration-300 cursor-pointer">{item.title}</h3>
+              <p className="text-sm text-gray-500 no-underline">{formatDateToString(item.created)}</p>
+              <p className="text-sm text-gray-500 no-underline">{ item.duration }</p>
+            </div>
+          </div>
+        ))
+      )}
       
     </>
   )
---
+++ src/utils/mockAPI/mockAPIVideos.js
@@ -0,0 +1,20 @@
+import { API_URL } from '../secrets.json';
+
+export async function mockAPIVideos({ limit = null, page = null  }) {
+  try {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    const fetchingApi = await fetch(`${API_URL}/xtape/videos?page=${page}`, requestOptions);
+    const responseData = await fetchingApi.json();
+
+    if (responseData.status === 'fail' || responseData.status === 'error') return { status: false, message: responseData.message };
+
+    return responseData;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
