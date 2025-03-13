5606efd8522fad86abc9615d8e69af4e41825eaa
Rezu0 <yunianrezky031@gmail.com>
2025-03-13 12:31:54 +0700

    add: penambahan untuk beberapa meta tag SEO

--- src/component/HomeComponent.jsx
+++ src/component/HomeComponent.jsx
@@ -7,11 +7,16 @@ function HomeComponent() {
   return (
     <>
       <Helmet>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Tonton pulugan ribu video berkualitas tinggi secara gratis! Nikmati streaming cepat tanpa buffering dengan koleksi Video 18+ HD terbaru. Temukan video favoritmu sekarang!" />
+        <meta name="keywords" content="streaming, 18+, bokep eropa, mia khalifa, bokep indonesia, video porno, adultdesu, adult, movie adult, netflix and chill, video eropa, video bokep, bokep streaming film dewasa, tontonan eksklusif, hiburan 18+, koleksi premium, video HD, film romantis, tayangan malam, video eksotis, streaming privat, konten khusus, film panas, adult streaming, video privat, nonton premium, film sensasi, hiburan malam, tontonan spesial, koleksi eksklusif, private movies, premium videos, film daring, tayangan khusus, high quality streaming, exclusive content, adult movies, entertainment 18+, private entertainment, premium collection"/>
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="Watch Free HD Porn - Adultdesu" />
+        <meta property="og:description" content="Tonton pulugan ribu video berkualitas tinggi secara gratis! Nikmati streaming cepat tanpa buffering dengan koleksi Video 18+ HD terbaru. Temukan video favoritmu sekarang!" />
+        <meta property="og:url" content="https://adultdesu.tv" />
         <title>Watch Free HD Porn - Adultdesu</title>
-        <meta 
-          name="description"
-          content="Website content 18+ dari Eropa dan sekitarnya"
-        />
+        <link rel="canonical" href="https://www.example.com" />
       </Helmet>
 
       <NavbarComponent />
