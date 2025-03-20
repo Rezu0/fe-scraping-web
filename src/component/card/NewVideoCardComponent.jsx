e40eb96d24e045339a21bd2950955f9adf9d1a6c
Rezu0 <yunianrezky031@gmail.com>
2025-03-20 15:33:16 +0700

    feat: add loading bar to NewVideoCardComponent for improved user experience during video loading

--- src/component/card/NewVideoCardComponent.jsx
+++ src/component/card/NewVideoCardComponent.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { mockAPIVideos } from '../../utils/mockAPI/mockAPIVideos';
 import { formatDateToString } from "../../utils/date/handlerDate";
+import { useNavigate } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 function NewVideoCardComponent() {
+  const navigate = useNavigate();
+  const [isProgressBar, setIsProgressBar] = useState(0);
   const [isContentVideos, setContentVideos] = useState([]);
   const [isLoading, setLoading] = useState(false);
 
@@ -26,8 +30,35 @@ function NewVideoCardComponent() {
     fetchContentVideos();
   }, [setContentVideos]);
 
+  const loadingBarState = async (dataNavigate) => {
+    setIsProgressBar((prev) => prev + 40);
+
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        setIsProgressBar((prev) => prev + 10);
+        resolve()
+      }, 500)
+    })
+
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        setIsProgressBar((prev) => prev + 50);
+        resolve()
+      }, 500)
+    })
+
+    setTimeout(() => {
+      navigate(dataNavigate);
+    }, 100);
+  }
+
   return (
     <>
+      <LoadingBar 
+       color="#E97991"
+       progress={isProgressBar}
+       onLoaderFinished={() => setIsProgressBar(0)}
+      />
       {isLoading ? (
         <div className="w-full text-center text-adultdesu-navbartext">
           Loading...
@@ -36,8 +67,9 @@ function NewVideoCardComponent() {
         isContentVideos?.map((item, index) => (
           <div
             key={index} 
-            className="w-full max-w-xs overflow-hidden group
-          ">
+            className="w-full max-w-xs overflow-hidden group"
+            onClick={async () => await loadingBarState(`/videos/${item.slug}`)}
+          >
             <img 
               srcSet={item.image}
               className="w-full h-36 md:h-48 object-cover rounded-md hover:opacity-50 cursor-pointer duration-300"
