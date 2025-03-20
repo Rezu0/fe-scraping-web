487cb87cee1faccd7d1bd2cc6796a545bda24460
Rezu0 <yunianrezky031@gmail.com>
2025-03-20 15:14:40 +0700

    fix: update mockAPIVideos function to include limit parameter in API request

--- src/utils/mockAPI/mockAPIVideos.js
+++ src/utils/mockAPI/mockAPIVideos.js
@@ -1,13 +1,13 @@
 import { API_URL } from '../secrets.json';
 
-export async function mockAPIVideos({ limit = null, page = null, tags = null, sort = null }) {
+export async function mockAPIVideos({ limit = '', page = '', tags = '', sort = '' }) {
   try {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow',
     };
 
-    const fetchingApi = await fetch(`${API_URL}/xtape/videos?page=${page}&tags=${tags}&sort=${sort}`, requestOptions);
+    const fetchingApi = await fetch(`${API_URL}/xtape/videos?page=${page}&tags=${tags}&sort=${sort}&limit=${limit}`, requestOptions);
     const responseData = await fetchingApi.json();
 
     if (responseData.status === 'fail' || responseData.status === 'error') return { status: false, message: responseData.message };
