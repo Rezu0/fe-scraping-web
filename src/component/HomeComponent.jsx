5dab995ba192e1a68594ccb6013058889fc756b5
Rezu0 <yunianrezky031@gmail.com>
2025-03-21 14:46:25 +0700

    fix: update meta tags for improved SEO and branding consistency

--- src/component/HomeComponent.jsx
+++ src/component/HomeComponent.jsx
@@ -14,20 +14,14 @@ function HomeComponent() {
         <meta name="description" content="Tonton pulugan ribu video berkualitas tinggi secara gratis! Nikmati streaming cepat tanpa buffering dengan koleksi Video 18+ HD terbaru. Temukan video favoritmu sekarang!" />
         <meta name="keywords" content="streaming, 18+, bokep eropa, mia khalifa, bokep indonesia, video porno, adultdesu, adult, movie adult, netflix and chill, video eropa, video bokep, bokep streaming film dewasa, tontonan eksklusif, hiburan 18+, koleksi premium, video HD, film romantis, tayangan malam, video eksotis, streaming privat, konten khusus, film panas, adult streaming, video privat, nonton premium, film sensasi, hiburan malam, tontonan spesial, koleksi eksklusif, private movies, premium videos, film daring, tayangan khusus, high quality streaming, exclusive content, adult movies, entertainment 18+, private entertainment, premium collection"/>
         <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="Watch Free HD Porn - Adultdesu" />
+        <meta property="og:title" content="Adultdesu - Watch Free HD Porn" />
         <meta property="og:description" content="Tonton pulugan ribu video berkualitas tinggi secara gratis! Nikmati streaming cepat tanpa buffering dengan koleksi Video 18+ HD terbaru. Temukan video favoritmu sekarang!" />
         <meta property="og:url" content="https://adultdesu.tv" />
         <title>Watch Free HD Porn - Adultdesu</title>
-        <link rel="canonical" href="https://www.example.com" />
+        <link rel="canonical" href="https://adultdesu.tv" />
       </Helmet>
 
       <NavbarComponent />
-
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <div className="bg-red-300 p-4 text-center">Kolom 1</div>
-        <div className="bg-red-400 p-4 text-center">Kolom 2</div>
-        <div className="bg-red-500 p-4 text-center">Kolom 3</div>
-      </div> */}
 
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
