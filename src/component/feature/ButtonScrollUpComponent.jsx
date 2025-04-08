7bba4840585807c5bec558f4b949a4c541601d5d
Rezu0 <yunianrezky031@gmail.com>
2025-04-08 16:35:05 +0700

    fix: update ButtonScrollUpComponent styles for improved visibility and consistency

--- src/component/feature/ButtonScrollUpComponent.jsx
+++ src/component/feature/ButtonScrollUpComponent.jsx
@@ -24,11 +24,12 @@ function ButtonScrollUpComponent() {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-5 right-5 p-3 bg-adultdesu-navbartext hover:bg-adultdesu-background-hover text-white rounded-full shadow-lg cursor-pointer transition-opacity duration-500 ${isVisible ? "opacity-100" : "opacity-0"}`}
+      className={`fixed bottom-0 right-20 py-2 px-3 bg-adultdesu-navbartext hover:bg-adultdesu-background-hover text-white rounded-t-md shadow-lg cursor-pointer transition-opacity duration-500 ${isVisible ? "opacity-100" : "opacity-0"}`}
     >
-      <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 15 7-7 7 7"/>
+      <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <path fillRule="evenodd" d="M5.575 13.729C4.501 15.033 5.43 17 7.12 17h9.762c1.69 0 2.618-1.967 1.544-3.271l-4.881-5.927a2 2 0 0 0-3.088 0l-4.88 5.927Z" clipRule="evenodd"/>
       </svg>
+
     </button>
   )
 }
