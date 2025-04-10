2f6232992099b6bcabc9933cb30da89ce1b472e1
Rezu0 <yunianrezky031@gmail.com>
2025-04-10 14:05:56 +0700

    feat: enhance NavbarComponent with active navigation state and update links for better user engagement

--- src/component/navbar/NavbarComponent.jsx
+++ src/component/navbar/NavbarComponent.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import SidebarComponent from "./SidebarComponent";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 import SearchBoxComponent from "../feature/SearchBoxComponent";
+import { classNames } from "primereact/utils";
 
 function NavbarComponent() {
   const [isSidebar, setIsSidebar] = useState(false);
@@ -10,10 +11,25 @@ function NavbarComponent() {
   const [isProgressBar, setIsProgressBar] = useState(0);
   const [isSearchQuery, setSearchQuery] = useState("");
   const [isSearchParams, setSearchParams] = useSearchParams();
+  const [isActiveNav, setActiveNav] = useState(null);
+
+  const location = useLocation();
   const navigate = useNavigate();
 
   const clickSidebarBurger = () => setIsSidebar((prev) => !prev);
   const clickSearchIcon = () => setIsSearch((prev) => !prev);
+
+  useEffect(() => {
+    if (location.pathname === '/categories') {
+      setActiveNav('categories');
+      return;
+    }
+
+    if (location.pathname === '/') {
+      setActiveNav('videos');
+      return;
+    }
+  }, [location.pathname])
 
   useEffect(() => {
     if (isSearchParams.has('search')) {
@@ -56,7 +72,7 @@ function NavbarComponent() {
   return (
     <>
       <LoadingBar 
-        color="#E97991"
+        color="#ad1320"
         progress={isProgressBar}
         onLoaderFinished={() => setIsProgressBar(0)}
       />
@@ -86,7 +102,7 @@ function NavbarComponent() {
                 </div>
                 <input 
                   type="search" 
-                  className="bg-gray-50 border-0 focus:outline-none focus:ring-0 text-gray-900 text-sm rounded-sm focus:ring-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-50 dark:placeholder-gray-400 dark:text-gray-800" 
+                  className="bg-gray-50 border-0 focus:outline-none focus:ring-0 text-gray-900 text-sm rounded-sm focus:ring-blue-500 block w-2xl ps-10 p-2 dark:bg-gray-50 dark:placeholder-gray-400 dark:text-gray-800" 
                   placeholder="Search your videos..."
                   value={isSearchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
@@ -95,38 +111,79 @@ function NavbarComponent() {
             </form>
           </div>
 
-          <div className="col-span-12 uppercase text-gray-50 font-medium text-xs">
+          <div className="hidden lg:flex col-span-12 uppercase text-gray-50 font-medium text-xs">
             <div className="flex flex-wrap gap-2">
-              <a
-                href=""
-                className="flex items-center gap-1 bg-adultdesu-backgroundbutton px-2 py-1 rounded-sm"
+              <div
+                className={classNames(
+                  (isActiveNav === 'videos') ? 'bg-adultdesu-backgroundbutton' : 'hover:bg-adultdesu-backgroundbutton',
+                  "flex items-center gap-1 transition duration-300 px-2 py-1 rounded-sm cursor-pointer"
+                )}
+                onClick={() => loadingBarState('/')}
               >
-                <svg
-                  className="w-5 h-5 text-gray-800 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z"
-                    clipRule="evenodd"
-                  />
+                <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M9 7V2.221a2 2 0 0 0-.5.365L4.586 6.5a2 2 0 0 0-.365.5H9Zm2 0V2h7a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9h5a2 2 0 0 0 2-2Zm-2 4a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2H9Zm0 2h2v2H9v-2Zm7.965-.557a1 1 0 0 0-1.692-.72l-1.268 1.218a1 1 0 0 0-.308.721v.733a1 1 0 0 0 .37.776l1.267 1.032a1 1 0 0 0 1.631-.776v-2.984Z" clipRule="evenodd"/>
                 </svg>
-                <span>Home</span>
-              </a>
+                <span>Videos</span>
+              </div>
 
-              <a
-                href=""
-                className="flex items-center gap-1 hover:bg-adultdesu-backgroundbutton transition duration-300 px-2 py-1 rounded-sm"
+              <div
+                className={classNames(
+                  (isActiveNav === 'categories') ? 'bg-adultdesu-backgroundbutton' : 'hover:bg-adultdesu-backgroundbutton',
+                  "flex items-center gap-1 transition duration-300 px-2 py-1 rounded-sm cursor-pointer"
+                )}
+                onClick={() => loadingBarState('/categories')}
               >
                 <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                   <path fillRule="evenodd" d="M3 6a2 2 0 0 1 2-2h5.532a2 2 0 0 1 1.536.72l1.9 2.28H3V6Zm0 3v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V9H3Z" clipRule="evenodd"/>
                 </svg>
                 <span>Categories</span>
+              </div>
+
+              <a
+                href="https://desu.bio/doujin"
+                className="flex items-center gap-1 hover:bg-adultdesu-backgroundbutton transition duration-300 px-2 py-1 rounded-sm"
+                target="_blank"
+              >
+                <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z" clipRule="evenodd"/>
+                </svg>
+
+                <span>Comics Hentai</span>
+              </a>
+
+              <a
+                href="https://desu.bio/jav"
+                className="flex items-center gap-1 hover:bg-adultdesu-backgroundbutton transition duration-300 px-2 py-1 rounded-sm"
+                target="_blank"
+              >
+                <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M9 7V2.221a2 2 0 0 0-.5.365L4.586 6.5a2 2 0 0 0-.365.5H9Zm2 0V2h7a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9h5a2 2 0 0 0 2-2Zm-2 4a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2H9Zm0 2h2v2H9v-2Zm7.965-.557a1 1 0 0 0-1.692-.72l-1.268 1.218a1 1 0 0 0-.308.721v.733a1 1 0 0 0 .37.776l1.267 1.032a1 1 0 0 0 1.631-.776v-2.984Z" clipRule="evenodd"/>
+                </svg>
+                <span>Jav</span>
+              </a>
+
+              <a
+                href="https://igodesu.tv/"
+                className="flex items-center gap-1 hover:bg-adultdesu-backgroundbutton transition duration-300 px-2 py-1 rounded-sm"
+                target="_blank"
+              >
+                <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M9 7V2.221a2 2 0 0 0-.5.365L4.586 6.5a2 2 0 0 0-.365.5H9Zm2 0V2h7a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9h5a2 2 0 0 0 2-2Zm-2 4a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2H9Zm0 2h2v2H9v-2Zm7.965-.557a1 1 0 0 0-1.692-.72l-1.268 1.218a1 1 0 0 0-.308.721v.733a1 1 0 0 0 .37.776l1.267 1.032a1 1 0 0 0 1.631-.776v-2.984Z" clipRule="evenodd"/>
+                </svg>
+                <span>Bokep Indo</span>
+              </a>
+
+              <a
+                href="https://t.me/iklandesu/"
+                className="flex items-center gap-1 hover:bg-adultdesu-backgroundbutton transition duration-300 px-2 py-1 rounded-sm"
+                target="_blank"
+              >
+                <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M12 14a3 3 0 0 1 3-3h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4a3 3 0 0 1-3-3Zm3-1a1 1 0 1 0 0 2h4v-2h-4Z" clipRule="evenodd"/>
+                  <path fillRule="evenodd" d="M12.293 3.293a1 1 0 0 1 1.414 0L16.414 6h-2.828l-1.293-1.293a1 1 0 0 1 0-1.414ZM12.414 6 9.707 3.293a1 1 0 0 0-1.414 0L5.586 6h6.828ZM4.586 7l-.056.055A2 2 0 0 0 3 9v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2h-4a5 5 0 0 1 0-10h4a2 2 0 0 0-1.53-1.945L17.414 7H4.586Z" clipRule="evenodd"/>
+                </svg>
+
+                <span>Pasang Iklan</span>
               </a>
             </div>
           </div>
